736e9b92a206ae142411b483da1202864f9e2347
leobez <leossbezerra@gmail.com>
2024-07-23 14:21:44 -0300

    Deleted projectsIndex

--- ProjectsIndex/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}", './src/*.{html}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-}
-
